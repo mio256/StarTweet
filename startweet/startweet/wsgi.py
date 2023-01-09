856a3e947b60ce104ed5d11e993e64e3569e9037
mio256 <reiya@woorth.io>
2023-01-09 19:39:42 +0900

    :wrench: set .env

--- startweet/startweet/wsgi.py
+++ startweet/startweet/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'startweet.settings')
 
 application = get_wsgi_application()
+
+load_dotenv(os.path.join('/home/mi0256/mi0256.pythonanywhere.com', '.env'))
