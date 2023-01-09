697acc035b82a9ea35ceacc04dc0b0444bd90249
mio256 <reiya@woorth.io>
2023-01-09 16:14:51 +0900

    :wrench::rocket: settings for first deploy

--- startweet/startweet/settings.py
+++ startweet/startweet/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = get_random_secret_key()  
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com', 'mi0256.pythonanywhere.com']
 
 
 # Application definition
@@ -129,3 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Session
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+
+# Deploy
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
