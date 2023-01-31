f568a969d2a06596bac671cf1254cef463a6e79f
mio256 <reiya@woorth.io>
2023-02-01 08:59:11 +0900

    :sparkles:

--- timeline/views.py
+++ timeline/views.py
@@ -1,4 +1,4 @@
-from .tweepy_func import create_client, like_user_tweets, rt_user_tweets, get_pinned_lists, page_home, page_user, page_list, like_home
+from . import tweepy_func
 
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
@@ -13,7 +13,7 @@ class TimelineViewBase(TemplateView):
         return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        client = create_client(self.request.session['access_token'], self.request.session['access_token_secret'])
+        client = tweepy_func.create_client(self.request.session['access_token'], self.request.session['access_token_secret'])
         if 'search_username' in request.POST:
             search_username = request.POST['search_username']
             id = client.get_user(username=str(search_username)).json()['data']['id']
@@ -21,15 +21,15 @@ class TimelineViewBase(TemplateView):
         elif 'like_username' in request.POST:
             like_username = request.POST['like_username']
             id = client.get_user(username=str(like_username)).json()['data']['id']
-            like_user_tweets(client, id, 20)
+            tweepy_func.like_user_tweets(client, id, 20)
             return redirect('timeline:user', id)
         elif 'rt_username' in request.POST:
             rt_username = request.POST['rt_username']
             id = client.get_user(username=str(rt_username)).json()['data']['id']
-            rt_user_tweets(client, id, 10)
+            tweepy_func.rt_user_tweets(client, id, 10)
             return redirect('timeline:user', id)
         elif 'like_home_limit' in request.POST:
-            like_home(client, request.POST['like_home_limit'])
+            tweepy_func.like_home(client, request.POST['like_home_limit'])
             return redirect('timeline:index')
 
 
@@ -37,11 +37,11 @@ class IndexView(TimelineViewBase):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        client = create_client(self.request.session['access_token'], self.request.session['access_token_secret'])
-        context['lists'] = get_pinned_lists(client)
+        client = tweepy_func.create_client(self.request.session['access_token'], self.request.session['access_token_secret'])
+        context['lists'] = tweepy_func.get_pinned_lists(client)
         context['me'] = client.get_me().json()['data']['id']
 
-        tweets = page_home(client, days=3)
+        tweets = tweepy_func.page_home(client, days=3)
         context['tweets'] = sorted(tweets, key=lambda x: -(x['public_metrics']['retweet_count'] * 2 + x['public_metrics']['like_count']))
 
         return context
@@ -51,12 +51,12 @@ class UserView(TimelineViewBase):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        client = create_client(self.request.session['access_token'], self.request.session['access_token_secret'])
-        context['lists'] = get_pinned_lists(client)
+        client = tweepy_func.create_client(self.request.session['access_token'], self.request.session['access_token_secret'])
+        context['lists'] = tweepy_func.get_pinned_lists(client)
         context['me'] = client.get_me().json()['data']['id']
 
         id = self.kwargs.get('id', '')
-        tweets = page_user(client, id, days=7)
+        tweets = tweepy_func.page_user(client, id, days=7)
         context['tweets'] = sorted(tweets, key=lambda x: -(x['public_metrics']['retweet_count'] * 2 + x['public_metrics']['like_count']))
 
         return context
@@ -66,12 +66,12 @@ class ListView(TimelineViewBase):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        client = create_client(self.request.session['access_token'], self.request.session['access_token_secret'])
-        context['lists'] = get_pinned_lists(client)
+        client = tweepy_func.create_client(self.request.session['access_token'], self.request.session['access_token_secret'])
+        context['lists'] = tweepy_func.get_pinned_lists(client)
         context['me'] = client.get_me().json()['data']['id']
 
         id = self.kwargs.get('id', '')
-        tweets = page_list(client, id, days=7)
+        tweets = tweepy_func.page_list(client, id, days=7)
         context['tweets'] = sorted(tweets, key=lambda x: -(x['public_metrics']['retweet_count'] * 2 + x['public_metrics']['like_count']))
 
         return context
